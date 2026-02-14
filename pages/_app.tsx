48ff9a354e1611a7614b47e6224bf43f97ddac8f
gaurang-ans <gaurang@anssoft.in>
2026-02-14 21:25:28 +0530

    making dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-200">
         <Navbar />
         <Component {...pageProps} />
